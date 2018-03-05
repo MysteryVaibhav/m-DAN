c256eea7de7609fb1da781d9385efbb4978f6cca
vaibhav <vvaibhav@andrew.cmu.edu>
2018-03-05 14:02:16 -0500

    Removed extra file

--- inspect_weights.py
+++
@@ -1,9 +0,0 @@
-import torch
-from model import mDAN
-
-model_1 = mDAN()
-model_1.load_state_dict(torch.load('model_weights.t7'))
-
-model_2 = mDAN()
-model_2.load_state_dict(torch.load('model_weights_1.t7'))
-print(".")
