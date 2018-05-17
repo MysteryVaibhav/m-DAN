66b8181b84fd7ed1e161c02a02181e9a015fe4bb
MysteryVaibhav <mysteryvaibhav@gmail.com>
2018-05-16 23:17:54 -0400

    Working old code

--- properties.py
+++ properties.py
@@ -11,15 +11,15 @@ MAX_CAPTION_LEN = 82 #49
 
 # DATA RELATED
 VISUAL_FEATURE_DIMENSION = 2048
-NO_OF_REGIONS_IN_IMAGE = 1 #14 * 14
+NO_OF_REGIONS_IN_IMAGE = 36 #14 * 14
 
 # MODEL 
-NO_OF_STEPS = 1
+NO_OF_STEPS = 2
 MARGIN = 100
 
 # PATH
 #TRAIN_IMAGES_DIR = '/data/disk1/junweil/vision_language/data/flickr30k/resnet-101/'
-TRAIN_IMAGES_DIR = '/data/disk1/junweil/vision_language/data/flickr30k/resnet-101_avg/'
-CAPTION_INFO = '/data/disk1/junweil/vision_language/data/flickr30k/results_20130124.token'
-SPLIT_INFO = '/data/disk1/junweil/vision_language/data/flickr30k/splits/'
-IMAGES_DIR = '/data/disk1/junweil/vision_language/data/flickr30k/flickr30k_images/'
+TRAIN_IMAGES_DIR = '/mnt/ssd1/junweil/vision_language/poyao_bottomup_feats/'
+CAPTION_INFO = '/mnt/ssd1/junweil/vision_language/results_20130124.token'
+SPLIT_INFO = '/mnt/ssd1/junweil/vision_language/splits/'
+IMAGES_DIR = '/mnt/ssd1/junweil/vision_language/flickr30k_images/'
--- trainer.py
+++ trainer.py
@@ -11,6 +11,9 @@ from process_data import run, get_ids
 from tqdm import tqdm
 
 
+kwargs = {'num_workers': 4, 'pin_memory': True} if torch.cuda.is_available() else {}
+
+
 class CustomDataSet(torch.utils.data.TensorDataset):
     def __init__(self, img_one_hot, ids, is_train):
         self.img_one_hot = img_one_hot
@@ -98,7 +101,7 @@ class CustomDataSet2(torch.utils.data.TensorDataset):
 def recall(model, img_one_hot):
     test_z_v = None  # no_of_images * visual_context_features
     plain_val_ids = get_ids('val', strip=True)
-    data_loader = torch.utils.data.DataLoader(CustomDataSet1(plain_val_ids), batch_size=BATCH_SIZE, shuffle=False)
+    data_loader = torch.utils.data.DataLoader(CustomDataSet1(plain_val_ids), batch_size=BATCH_SIZE, shuffle=False, **kwargs)
     for (caption_, mask_, image_) in data_loader:
         _, _, z_v = model(to_variable(caption_),
                           to_variable(mask_),
@@ -110,7 +113,7 @@ def recall(model, img_one_hot):
     test_z_v = test_z_v.numpy()
 
     val_ids = get_ids('val')
-    data_loader = torch.utils.data.DataLoader(CustomDataSet2(img_one_hot, val_ids), batch_size=BATCH_SIZE, shuffle=False)
+    data_loader = torch.utils.data.DataLoader(CustomDataSet2(img_one_hot, val_ids), batch_size=BATCH_SIZE, shuffle=False, **kwargs)
     r_1 = 0
     r_5 = 0
     r_10 = 0
@@ -161,7 +164,7 @@ def train():
     img_one_hot = run()
     train_ids = get_ids('train')
     train_data_loader = torch.utils.data.DataLoader(CustomDataSet(img_one_hot, train_ids, True), batch_size=BATCH_SIZE,
-                                                    shuffle=True)
+                                                    shuffle=True, **kwargs)
     model = mDAN()
     model.apply(init_xavier)
     loss_function = margin_loss()
@@ -190,7 +193,7 @@ def train():
                 neg_mask = neg_mask.cuda()
                 neg_image = neg_image.cuda()
             
-            #caption, mask, image, neg_cap, neg_mask, neg_image = hard_negative_mining(model, caption, mask, image, neg_cap, neg_mask, neg_image)
+            caption, mask, image, neg_cap, neg_mask, neg_image = hard_negative_mining(model, caption, mask, image, neg_cap, neg_mask, neg_image)
             optimizer.zero_grad()
             # Run our forward pass.
             similarity = model(torch.autograd.Variable(caption), torch.autograd.Variable(mask), torch.autograd.Variable(image), False)
